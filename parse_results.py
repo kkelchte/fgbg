7fdf0ce7ae6686c0dacae40095c45de1aae38a35
kelchtermans <kkelchtermans@gmail.com>
2021-06-08 17:51:38 +0200

    parse results and ignore output

--- parse_results.py
+++ parse_results.py
@@ -2,19 +2,17 @@ import os
 import shutil
 from glob import glob
 
-data_dir = "data"
+# data_dir = "data"
+data_dir = "/Users/kelchtermans/mount/esat/code/contrastive-learning/data"
 TARGETS = ["cone", "gate", "line"]
 CONFIGS = [
-    f"configs/{cf}.json"
-    for cf in [
-        "vanilla",
-        "default",
-        "default_triplet",
-        "deep_supervision",
-        "deep_supervision_triplet",
-        "deep_supervision_blur",
-        "deep_supervision_triplet_blur",
-    ]
+    "vanilla",
+    "default",
+    "default_triplet",
+    "deep_supervision",
+    "deep_supervision_triplet",
+    "deep_supervision_blur",
+    "deep_supervision_triplet_blur",
 ]
 
 print("TARGETS: ", TARGETS)
@@ -43,7 +41,7 @@ for target in TARGETS:
             for lrp in lr_paths
         }
         validation_losses = {
-            lrp: values[lrp]["validation_mse_loss_avg"] for lrp in lr_paths
+            lrp: values[lrp]["validation_bce_loss_avg"] for lrp in lr_paths
         }
         best_lrp = [
             k for k, v in sorted(validation_losses.items(), key=lambda item: item[1])
@@ -54,23 +52,32 @@ for target in TARGETS:
 # Print table and store to file:
 
 overview_file = open("overview_table.txt", "w")
-for conf in CONFIGS:
-    for target in ["cone", "gate", "line"]:
-        msg = f'{os.path.basename(conf)[:-4].replace("_", " ")} '
-        msg += f'| {overview_results[target][conf]["validation_iou_avg"]} '
-        msg += f'(±{overview_results[target][conf]["validation_iou_std"]}) | '
-        msg += f'{overview_results[target][conf]["out-of-distribution_iou_avg"]} '
-        msg += f'(±{overview_results[target][conf]["out-of-distribution_iou_std"]}) \\'
-        print(msg)
-        overview_file.write(msg + "\n")
+for target in ["cone", "gate", "line"]:
+    for conf in CONFIGS:
+        try:
+            msg = f'{os.path.basename(conf).replace("_", " ")} '
+            msg += f'&  {overview_results[target][conf]["validation_iou_avg"]} '
+            msg += f'(±{overview_results[target][conf]["validation_iou_std"]}) & '
+            msg += f'{overview_results[target][conf]["out-of-distribution_ious_avg"]} '
+            msg += (
+                f'(±{overview_results[target][conf]["out-of-distribution_ious_std"]})'
+            )
+            msg += " \\\\"
+            print(msg)
+            overview_file.write(msg + "\n")
+        except KeyError:
+            print(f"Failed to parse {conf}/{target}")
 overview_file.close()
 
 # Copy winning real images for quantitative results
 image_dir = "overview_real_imgs"
-os.makedirs(image_dir, exist_ok=False)
+os.makedirs(image_dir, exist_ok=True)
 for target in TARGETS:
     for conf in CONFIGS:
-        shutil.copyfile(
-            winning_lrs[target][conf] + "/imgs/real_0.jpg",
-            f"{image_dir}/{target}_{os.path.basename(conf)[:-4]}.jpg",
-        )
+        try:
+            shutil.copyfile(
+                winning_lrs[target][conf] + "/imgs/real_0.jpg",
+                f"{image_dir}/{target}_{os.path.basename(conf)}.jpg",
+            )
+        except FileNotFoundError:
+            print(f"Failed to copy from {winning_lrs[target][conf]}")
