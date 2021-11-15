6f64000645315ed6e7a330f2e49838243b4b2cb5
kkelchte <kkelchtermans@gmail.com>
2021-11-15 12:42:53 +0100

    ds config

--- condor_launcher_down_stream.py
+++ condor_launcher_down_stream.py
@@ -7,7 +7,6 @@ import shlex
 
 INTERPRETER_PATH = "/esat/opal/kkelchte/conda/envs/venv/bin/python"
 PROJECT_PATH = "/users/visics/kkelchte/code/contrastive-learning"
-OUTPUT_PATH = "data/down_stream"
 
 SPECS = {
     "Universe": "vanilla",
@@ -21,42 +20,26 @@ SPECS = {
     "Niceuser": "True",
     "+RequestWalltime": int(50 * 3 * 60),
 }
-NUMEPOCH = 50
-# TARGETS = ["cone", "gate", "line"]
-TARGETS = ["gate"]
-# TARGETS = ["red_line", "line"]
-TASKS = ["waypoints", "velocities"]
-TEXTURE_DIR = {
-    "cone": "data/datasets/dtd",
-    "line": "data/datasets/dtd_and_places",
-    "red_line": "data/datasets/dtd_and_places",
-    "gate": "data/datasets/dtd",
-}
-CONFIGS = {
-    "cone": "default",
-    "line": "deep_supervision_fg",
-    "red_line": "deep_supervision_fg",
-    "gate": "deep_supervision_fg",
-}
-ENCODERS = {
-    "cone": "data/dtd_and_places_augmented/default/cone/best",
-    "line": "data/dtd_and_places_augmented/pretrain/deep_supervision_fg/line/best",
-    "red_line": "data/dtd_and_places_augmented/pretrain/deep_supervision_fg/red_line/best",
-    "gate": "data/dtd_augmented/pretrain/deep_supervision_fg/gate/best",
-}
 
-# LEARNING_RATES = [0.001, 0.0001, 0.00001]
-LEARNING_RATES = [0.00001]
+# RED_LINE
+TARGET = "red_line"
+CONFIG = "???"
+
+# GATE
+# TARGET = 'gate'
+# CONFIG = '???'
+
+ENCODER = f"data/{TARGET}/{CONFIG}"
+TEXTURE_DIR = "data/datasets/dtd_and_places"
+TASKS = ["waypoints", "velocities"]
+LEARNING_RATE = 0.00001
 SUBMIT = True
 RM_EXIST = True
-
-print("TARGETS: ", TARGETS)
-print("TASKS: ", TASKS)
-print("LEARNING_RATES: ", LEARNING_RATES)
+NUMEPOCH = 50
 
 
 def create_condor_job_file(trgt, task, lrate):
-    output_dir = f"{OUTPUT_PATH}/{task}/{trgt}/{lrate}"
+    output_dir = f"data/{trgt}/{CONFIG}/{task}"
     if RM_EXIST and os.path.isdir(output_dir):
         shutil.rmtree(output_dir)
     os.makedirs(output_dir)
@@ -64,10 +47,10 @@ def create_condor_job_file(trgt, task, lrate):
         jobfile.write(f"executable     = {INTERPRETER_PATH} \n")
         jobfile.write(
             f"arguments = {PROJECT_PATH}/run.py --config_file "
-            f"{PROJECT_PATH}/configs/{CONFIGS[trgt]}.json "
+            f"{PROJECT_PATH}/configs/{CONFIG}.json "
             f"--learning_rate {lrate} --target {trgt} "
             f"--output_dir {output_dir} --texture_directory {TEXTURE_DIR[trgt]} "
-            f"--encoder_ckpt_dir {ENCODERS[trgt]} "
+            f"--encoder_ckpt_dir {ENCODER} "
             f"--task {task} --number_of_epochs {NUMEPOCH} \n"
         )
 
@@ -83,15 +66,10 @@ def create_condor_job_file(trgt, task, lrate):
 
 
 for task in TASKS:
-    for target in TARGETS:
-        for lr in LEARNING_RATES:
-            filename = create_condor_job_file(target, task, lr)
-            if SUBMIT:
-                print(f"submitting {filename}")
-                subprocess.call(shlex.split(f"condor_submit {filename}"))
-                time.sleep(0.1)
-    # wait 20 minutes
-    # if SUBMIT and len(CONFIGS) != 1:
-    #    time.sleep(10 * 60)
+    filename = create_condor_job_file(TARGET, task, LEARNING_RATE)
+    if SUBMIT:
+        print(f"submitting {filename}")
+        subprocess.call(shlex.split(f"condor_submit {filename}"))
+        time.sleep(0.1)
 
 print("finished")
